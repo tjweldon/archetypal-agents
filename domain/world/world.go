1c1f2361d4c4ae71240d0431f680684b77d0cadd
tjweldon <tomd90@gmail.com>
2022-04-24 19:59:16 +0100

    Improved comments and added vector methods

--- domain/world/world.go
+++ domain/world/world.go
@@ -2,26 +2,27 @@ package world
 
 import "math"
 
-// Sum is the generalisation of + on real numbers. An implementation should have the following characteristics:
-//  - Sum(a, b) == Sum(b, a)
-//  - Sum(Sum(a, b), c) == Sum(a, Sum(b, c))
-//  - Sum(0, a) == a
-//  - Sum(-a, a) == 0
+// Sum is the generalisation of + on real numbers. An implementation should have the following
+// properties:
+//  - Commutativity: 		Sum(a, b) == Sum(b, a)
+//  - Associativity:		Sum(Sum(a, b), c) == Sum(a, Sum(b, c)) === Sum(a, b, c)
+//  - A Zero value: 		Sum(0, a) == a
+//  - Existence of Inverse: Sum(-a, a) == 0
 type Sum func(scalars ...float64) float64
 
-// Metric is the generalisation of the absolute distance between two points in a connected 1d space. Any implementation
-// should have the following properties:
-//  - Metric(a, b) == Metric(b, a)
-//  - Metric(a, b) >= 0
-//  - Metric(a, a) == 0
-//  - Metric(a, b) + Metric(b, c) >= Metric(a, c)
+// Metric is the generalisation of the absolute distance between two points in a connected 1d space.
+// Any implementation should have the following properties:
+//  - Symmetry: 			Metric(a, b) == Metric(b, a)
+//  - Positivity: 			Metric(a, b) >= 0
+//  - Minimum: 				Metric(a, a) == 0
+//  - Triangle Inequality: 	Metric(a, b) + Metric(b, c) >= Metric(a, c)
 type Metric func(scalarA, scalarB float64) float64
 
 // Invert is the generalisation of changing the sign of a value, i.e. finding the inverse under addition.
 // An implementation should have the following properties:
-//  - Sum(Invert(a), a) == 0
-//  - Invert(0) == 0
-//  - Invert(Invert(a)) == a
+//  - Inversion under Sum: 			Sum(Invert(a), a) == 0
+//  - Fixed Point is 0: 			Invert(0) == 0
+//  - Invert^2 === Identity: 		Invert(Invert(a)) == a
 type Invert func(scalar float64) float64
 
 // MetricSpace1D is a representation of the way distance is calculated in a given coordinate.
@@ -32,8 +33,8 @@ type MetricSpace1D struct {
 	Invert Invert
 }
 
-// Line returns a MetricSpace1D that behaves like the usual real numbers unbounded above and below
-func Line() MetricSpace1D {
+// RealLine returns a MetricSpace1D that behaves like the usual real numbers unbounded above and below
+func RealLine() MetricSpace1D {
 	return MetricSpace1D{
 		Sum: func(scalars ...float64) float64 {
 			sum := 0.0
@@ -51,9 +52,11 @@ func Line() MetricSpace1D {
 	}
 }
 
-// Circles returns a MetricSpace1D that behaves like a circle with the circumference provided (think of an angle parameter)
+// Circles returns a MetricSpace1D that behaves like a circle with the circumference provided.
+// Think of the coordinate as an angle parameter, but not normalised by radius,
+// i.e. max(angle) == circumference.
 func Circles(circumference float64) MetricSpace1D {
-	line := Line()
+	line := RealLine()
 	return MetricSpace1D{
 		Sum: func(scalars ...float64) float64 {
 			return math.Remainder(line.Sum(scalars...), circumference)
@@ -61,7 +64,7 @@ func Circles(circumference float64) MetricSpace1D {
 		Metric: func(scalarA, scalarB float64) float64 {
 			result := math.Remainder(line.Metric(scalarA, scalarB), circumference)
 			if result > circumference/2 {
-				result = circumference/2 - result
+				result = circumference - result
 			}
 			return math.Abs(result)
 		},
@@ -78,30 +81,33 @@ type MetricSpace2D struct {
 
 // GeodesicDiff returns a tuple of geodesic distances between two points (x1, y1) and (x2, y2).
 // This defaults to the shortest straight path between the points. This is useful in toroidal topologies.
+// The result retains the sign of the difference as well as the magnitude
 func (m MetricSpace2D) GeodesicDiff(x1, x2, y1, y2 float64) (deltaX, deltaY float64) {
 	deltaX = m.XCoord.Sum(m.XCoord.Invert(x1), x2)
-	deltaY = m.YCoord.Metric(m.YCoord.Invert(y1), y2)
+	deltaY = m.YCoord.Sum(m.YCoord.Invert(y1), y2)
 	return deltaX, deltaY
 }
 
-// Metric is the function that defines the distance between two points represented by the Vector pair (v1, v2)
+// Metric is the function that defines the distance between two points represented by the Vector
+// pair (v1, v2)
 func (m *MetricSpace2D) Metric(v1, v2 *Vector) float64 {
 	deltaX, deltaY := m.GeodesicDiff(v1.X, v2.X, v1.Y, v2.Y)
 	return math.Sqrt(math.Pow(deltaX, 2) + math.Pow(deltaY, 2))
 }
 
-// NewEuclideanPlane initialises a MetricSpace2D that represents euclidean geometry and non-periodic boundary conditions i.e. an
-// infinite 2D plane
+// NewEuclideanPlane initialises a MetricSpace2D that represents euclidean geometry and non-periodic
+// boundary conditions i.e. an infinite 2D plane
 func NewEuclideanPlane() *MetricSpace2D {
-	line := Line()
+	line := RealLine()
 	return &MetricSpace2D{
 		XCoord: line,
 		YCoord: line,
 	}
 }
 
-// NewEuclideanToroid initialises a MetricSpace2D that represents geometry and periodic boundary conditions on both axes i.e. a torus.
-// This corresponds to agents leaving the screen on one boundary reappearing at the opposite boundary.
+// NewEuclideanToroid initialises a MetricSpace2D that represents geometry and periodic boundary
+// conditions on both axes i.e. a torus. This corresponds to agents leaving the screen on one
+// boundary reappearing at the opposite boundary.
 func NewEuclideanToroid(w, h float64) *MetricSpace2D {
 	return &MetricSpace2D{
 		XCoord: Circles(w),
@@ -119,14 +125,14 @@ func (m *MetricSpace2D) NewVector(x, y float64) *Vector {
 	return &Vector{metricSpace: m, X: x, Y: y}
 }
 
-// Vector represents a point in a space and can be summed in a way that respects linearity. This is a mutable implementation because
-// I don't want to get a memory out.
+// Vector represents a point in a space and can be summed in a way that respects linearity.
+// This is a mutable implementation because I don't want to get a memory out.
 type Vector struct {
 	metricSpace *MetricSpace2D
 	X, Y        float64
 }
 
-// Accumulate is a mutating variadic sum
+// Accumulate is a mutating variadic sum. This is the mutable equivalent of Sum
 func (v *Vector) Accumulate(vectors ...*Vector) *Vector {
 	xScalars, yScalars := make([]float64, len(vectors)), make([]float64, len(vectors))
 	for index, summand := range vectors {
@@ -138,7 +144,50 @@ func (v *Vector) Accumulate(vectors ...*Vector) *Vector {
 	return v
 }
 
+// Times is an **immutable** scalar multiple of the vector instance where the semantics are:
+// u = v.Times(x) => u != v
+func (v *Vector) Times(scalar float64) Vector {
+	x := v.metricSpace.XCoord.Sum(scalar * v.X)
+	y := v.metricSpace.YCoord.Sum(scalar * v.Y)
+	return Vector{v.metricSpace, x, y}
+}
+
+// Scale is a **mutating** scalar multiple of the vector instance with the following semantics:
+// u = v.Scale(x); u == v
+func (v *Vector) Scale(scalar float64) *Vector {
+	v.X = v.metricSpace.XCoord.Sum(scalar * v.X)
+	v.Y = v.metricSpace.YCoord.Sum(scalar * v.Y)
+	return v
+}
+
+// Minus is an **immutable** operation where the semantics are:
+// u = v.Minus(w) => u != v
 func (v *Vector) Minus(vector *Vector) Vector {
 	deltaX, deltaY := v.metricSpace.GeodesicDiff(v.X, vector.X, v.Y, vector.Y)
 	return Vector{X: deltaX, Y: deltaY}
+}
+
+// Subtract is a **mutable** operation where the semantics are:
+// u = v.Subtract(w) => u == v
+func (v *Vector) Subtract(vector *Vector) *Vector {
+	v.X, v.Y = v.metricSpace.GeodesicDiff(v.X, vector.X, v.Y, vector.Y)
+	return v
+}
+
+// Mag returns the length of the vector i.e. the geodesic distance from the head of
+// the vector to the origin.
+func (v *Vector) Mag() float64 {
+	return v.metricSpace.Metric(v, v.metricSpace.ZeroVector())
+}
+
+// Dot is the scalar product of two vectors with the following semantics:
+// x = u ⋅ v <=> x := u.Dot(v)
+//
+// Dot should have the following properties:
+//  - u.Dot(v) == 0.0 				<=> u perpendicular to v
+//  - u.Dot(v) == u.Mag()*v.Mag() 	<=> u parallel to v
+//    -	therefore:					==> math.Sqrt(u.Dot(u)) === u.Mag()
+func (v *Vector) Dot(w *Vector) float64 {
+	norm := v.metricSpace.XCoord.Sum
+	return norm(v.X)*norm(w.X) + norm(v.Y)*norm(w.Y)
 }
